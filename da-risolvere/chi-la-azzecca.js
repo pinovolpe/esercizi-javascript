103b31de3769654963674ad1c17047b9e43222b9
Pino Volpe <71434821+pinovolpe@users.noreply.github.com>
2020-10-26 16:44:48 +0100

    risolto

--- da-risolvere/chi-la-azzecca.js
+++ da-risolvere/chi-la-azzecca.js
@@ -19,3 +19,56 @@
 
   http://www.imparareaprogrammare.it
 */
+
+// ho fatto una piccola modifica al compito per renderlo più interessante, aggiornando più volte la pagina si apprezza la logica del programma:
+// i numeri sono da 1 a 6 (come al gioco dei dadi), casuali sia per i giocatori che per il banco
+
+var dado = Math.floor(Math.random() * (6-1) + 1);
+var giocatore1 = {
+  nome : 'Giocatore A',
+  punteggio : Math.floor(Math.random() * (6-1) + 1)
+};
+var giocatore2 = {
+  nome : 'Giocatore B',
+  punteggio : Math.floor(Math.random() * (6-1) + 1)
+};
+
+// counts e closest non sono farina del mio sacco, ho dovuto cercare su stackoveflow :'(
+
+var counts = [giocatore1.punteggio, giocatore2.punteggio];
+var closest = counts.reduce(function(prev, curr) {
+  return (Math.abs(curr - dado) < Math.abs(prev - dado) ? curr : prev);
+});
+
+// le righe che seguono sì però, le ho scritte io! :)
+
+if (dado == giocatore1.punteggio == giocatore2.punteggio) {
+  document.write(`Banco = ${dado}, ${giocatore1.nome} = ${giocatore1.punteggio}, ${giocatore2.nome} = ${giocatore2.punteggio}`);
+  document.write(`<br>PAREGGIO!`);
+}
+else if (dado == giocatore1.punteggio) {
+  document.write(`Banco = ${dado}, ${giocatore1.nome} = ${giocatore1.punteggio}, ${giocatore2.nome} = ${giocatore2.punteggio}`);
+  document.write(`<br>${giocatore1.nome} VINCE!`);
+}
+else if (dado == giocatore2.punteggio) {
+  document.write(`Banco = ${dado}, ${giocatore1.nome} = ${giocatore1.punteggio}, ${giocatore2.nome} = ${giocatore2.punteggio}`);
+  document.write(`<br>${giocatore2.nome} VINCE!`);
+}
+else {
+  document.write(`Banco = ${dado}, ${giocatore1.nome} = ${giocatore1.punteggio}, ${giocatore2.nome} = ${giocatore2.punteggio}`);
+  document.write(`<br>Banco VINCE!`);
+  switch (closest) {
+    case giocatore1.punteggio:
+      if (giocatore1.punteggio == giocatore2.punteggio) {
+        document.write(`<br>Entrambi i giocatori hanno totalizzato lo stesso punteggio!`);
+      }
+      else document.write(`<br>Tra ${giocatore1.punteggio} e ${giocatore2.punteggio} il numero più vicino a ${dado} è ${closest}, per cui ${giocatore1.nome} si è avvicinato di più!`);
+    break;
+    case giocatore2.punteggio:
+      if (giocatore1.punteggio == giocatore2.punteggio) {
+        document.write(`<br>Entrambi i giocatori hanno totalizzato lo stesso punteggio!`);
+      }
+      else document.write(`<br>Tra ${giocatore1.punteggio} e ${giocatore2.punteggio} il numero più vicino a ${dado} è ${closest}, per cui ${giocatore2.nome} si è avvicinato di più!`);
+    break;
+  }
+}
