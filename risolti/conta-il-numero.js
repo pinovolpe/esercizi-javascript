0200f7a4b8a716f63dcae70957da1342419556e5
Pino Volpe <71434821+pinovolpe@users.noreply.github.com>
2020-12-14 14:20:26 +0100

    risolto

---
+++ risolti/conta-il-numero.js
@@ -0,0 +1,29 @@
+/*
+  Conta il numero
+  Scrivi una funzione che dato un numero intero (massimo 9999) conti da quante cifre è formato.
+
+  Esempi:
+    Input: numero = 9
+    Output: 1 cifra
+
+    Input: numero = 245
+    Output: 3 cifre
+
+    
+  http://www.imparareaprogrammare.it
+*/
+
+function cifre() {
+  let n = Math.floor(Math.random() * 9999);
+  document.write(n);
+  console.log(n.toString().length);
+}
+
+// Oppure, si ha lo stesso risultato con:
+// const cifre = function () {
+//     let n = Math.floor(Math.random() * 9999);
+//     document.write(n);
+//     console.log(n.toString().length);
+// }
+
+cifre();
