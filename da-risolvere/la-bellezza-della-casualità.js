16287a3ec85e84a3b59db677cfff7183800b101e
Pino Volpe <71434821+pinovolpe@users.noreply.github.com>
2023-04-03 16:28:32 +0200

    Risolto - La bellezza della casualità

--- da-risolvere/la-bellezza-della-casualità.js
+++ da-risolvere/la-bellezza-della-casualità.js
@@ -8,3 +8,35 @@
 
   http://www.imparareaprogrammare.it
 */
+
+    <script>
+      // var numeroInput = prompt("Il numero limite");
+      // var numeroCasuale = Math.floor(Math.random() * (numeroInput - 1) + 1);
+
+      // console.log(
+      //   "numero Input:",
+      //   numeroInput,
+      //   " - ",
+      //   "numero Casuale:",
+      //   numeroCasuale
+      // );
+
+      function casuali() {
+        numeroInput = parseInt(document.getElementById("quanti").value);
+        var numeroCasuale = Math.floor(Math.random() * (numeroInput - 1) + 1);
+
+        document.getElementById("casuali").innerHTML =
+          "Numero limite:" +
+          numeroInput +
+          " - " +
+          "Numero casuale:" +
+          numeroCasuale;
+      }
+    </script>
+
+    <p>
+      Numero limite:
+      <input type="text" id="quanti" />
+      <input type="button" onclick="casuali()" value="Randomize!" />
+    </p>
+    <p id="casuali"></p>
